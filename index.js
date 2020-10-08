e56aaee9ea893e706f52471a6a3669fb8073d3ff
Justin Justin <jbry0723@gmail.com>
2020-10-08 19:59:50 -0400

    Completed all non-stretch tasks

--- index.js
+++ index.js
@@ -139,9 +139,12 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(arr) {
+  let ind=(arr.length-1);
+  return `${arr[ind].name} gave the restaurant a ${arr[ind].rating} star review, and their feedback was: ${arr[ind].feedback}`
 } 
+console.log (getLastReview(reviews))
+
 
 
 
